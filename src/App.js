f90cb84292ec0a7b4a75fa3dc2d9e3eeac88872a
Saurav Kumar <golukumar806@gmail.com>
2023-09-21 22:14:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+imp
 
 function App() {
   return (
     <div>
-      <Header />
-      <Note />
+      <Header />ort myNote from "./components/myNote";
+      {myNote.map(note => (
+        <Note 
+          key = {note.id}
+          title = {note.title}
+          desc = {note.desc}
+        />
+      ))}
       <Footer />
     </div>
   );
---
+++ src/components/myNote.js
@@ -0,0 +1,35 @@
+const myNote = [
+    {
+        id : 1,
+        title : "Note 1",
+        desc : "Decs 1"
+    },
+    {
+        id : 2,
+        title : "Note 2",
+        desc : "Decs 2"
+    },
+    {
+        id : 3,
+        title : "Note 3",
+        desc : "Decs 3"
+    },
+    {
+        id : 4,
+        title : "Note 4",
+        desc : "Decs 4"
+    },
+    {
+        id : 4,
+        title : "Note 4",
+        desc : "Decs 4"
+    },
+    {
+        id :5,
+        title: "shubh",
+        desc: "kuch nhi"
+    }
+
+];
+
+export default myNote;
